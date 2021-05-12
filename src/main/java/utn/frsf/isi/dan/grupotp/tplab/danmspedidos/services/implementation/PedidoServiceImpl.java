502a430cfc9be329c8abc5a16805f10936b4be6d
Hernan Albornoz <calbornoz@frsf.utn.edu.ar>
2021-05-11 22:37:52 -0300

    Avances en implementacion de PedidoServiceImpl

--- src/main/java/utn/frsf/isi/dan/grupotp/tplab/danmspedidos/services/implementation/PedidoServiceImpl.java
+++ src/main/java/utn/frsf/isi/dan/grupotp/tplab/danmspedidos/services/implementation/PedidoServiceImpl.java
@@ -1,36 +1,49 @@
 package utn.frsf.isi.dan.grupotp.tplab.danmspedidos.services.implementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import utn.frsf.isi.dan.grupotp.tplab.danmspedidos.model.DetallePedido;
 import utn.frsf.isi.dan.grupotp.tplab.danmspedidos.model.Pedido;
+import utn.frsf.isi.dan.grupotp.tplab.danmspedidos.repository.DetallePedidoRepository;
+import utn.frsf.isi.dan.grupotp.tplab.danmspedidos.repository.PedidoRepository;
 import utn.frsf.isi.dan.grupotp.tplab.danmspedidos.services.PedidoService;
 
 import java.util.List;
 import java.util.Optional;
 
 public class PedidoServiceImpl implements PedidoService {
+    private final PedidoRepository pedidoRepository;
+    private final DetallePedidoRepository detallePedidoRepository;
+
+    @Autowired
+    public PedidoServiceImpl(PedidoRepository pedidoRepository, DetallePedidoRepository detallePedidoRepository){
+        this.pedidoRepository=pedidoRepository;
+        this.detallePedidoRepository=detallePedidoRepository;
+    }
+
     @Override
     public List<Pedido> buscarTodos() {
-        return null;
+        return pedidoRepository.findAll();
     }
 
     @Override
     public Optional<Pedido> buscarPedidoPorId(Integer id) {
-        return Optional.empty();
+        return pedidoRepository.findById(id);
     }
 
     @Override
     public Optional<List<Pedido>> buscarPedidosPorIdObra(Integer id) {
-        return Optional.empty();
+        return pedidoRepository.findByIdObra(id);
     }
 
     @Override
     public Optional<List<Pedido>> buscarPedidosPorCliente(Integer id, String cuit) {
+        //TODO hacer bien esto
         return Optional.empty();
     }
 
     @Override
     public Optional<DetallePedido> buscarDetallePedidoPorId(Integer idPedido, Integer id) {
-        return Optional.empty();
+        return detallePedidoRepository.findById(id);
     }
 
     @Override
