c7e85a3d3968405b433085255fe928727428ce6a
Hernan Albornoz <calbornoz@frsf.utn.edu.ar>
2021-05-11 21:06:33 -0300

    Initial commit

---
+++ src/main/java/utn/frsf/isi/dan/grupotp/tplab/danmspedidos/MSPedidosApplication.java
@@ -0,0 +1,13 @@
+package utn.frsf.isi.dan.grupotp.tplab.danmspedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utn/frsf/isi/dan/grupotp/tplab/danmspedidos/MSPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.frsf.isi.dan.grupotp.tplab.danmspedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
